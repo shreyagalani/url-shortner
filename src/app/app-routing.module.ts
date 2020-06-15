e1b224794f17d0db6c014b0d0d0c8cc77d2df083
Shreya Galani <shreya.galani@theredpandas.com>
2020-06-15 18:33:48 +0530

    initial working app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [];
-
+const urls =[];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  addUrl(newUrl: string) {
+    if (newUrl) {
+      urls.push(newUrl);
+    }
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+
+const  requestHeaders = {
+  "Content-Type": "application/json",
+  "apikey": "8f9c5e0fcc4549d3bb2742188b0d43e4"
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'url-shortner';
+  urls =[];
+  addUrl(newUrl: string) {
+    let linkRequest = {
+      destination: newUrl,
+      domain: { fullName: "rebrand.ly" }
+      //, slashtag: "A_NEW_SLASHTAG"
+      //, title: "Rebrandly YouTube channel"
+    }
+    if (newUrl) {
+      axios({
+        method:'post',
+        url:'https://api.rebrandly.com/v1/links',
+        data:JSON.stringify(linkRequest),
+        headers:requestHeaders
+      }).then((response)=>{
+        this.urls.push({displayValue:response.data.shortUrl,actualValue: `https://${response.data.shortUrl}`});
+      }).catch((error)=>{
+        console.log('Error occured',error)
+      })
+    }
+  }
+  goToLink(url: string){
+    console.log('url',url)
+    window.open(url, "_blank");
+}
 }
