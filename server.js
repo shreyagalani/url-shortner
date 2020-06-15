25e7cb0ffb931ec906f77e3016d1d70a9258a6bc
Shreya Galani <shreya.galani@theredpandas.com>
2020-06-15 10:27:26 +0530

    modified name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/intrinsik-url-shortner-app'));
+app.use(express.static(__dirname + '/dist/url-shortner'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/intrinsik-url-shortner-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/url-shortner/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
